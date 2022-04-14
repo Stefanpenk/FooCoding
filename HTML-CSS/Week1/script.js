f91ed348a88bfea5e484410adfd05abe3001c0e5
Stefan Penk <stefan.penk@gmail.com>
2022-04-14 19:49:21 +0200

    Button Dark Mode update

--- HTML-CSS/Week1/script.js
+++ HTML-CSS/Week1/script.js
@@ -1,23 +1,42 @@
+//LONG VERSION
+// const btn = document.querySelector("button");
+// const body = document.querySelector("body");
+// let check = false;
+
+// function addDarkMode() {
+//     body.classList.add("darkMode");
+//     btn.textContent = "Light Mode";
+// }
+
+// function RemoveDarkMode() {
+//     body.classList.remove("darkMode");
+//     btn.textContent = "Dark Mode";
+// }
+
+// function handleBtn() {
+//     check = !check
+//     if (check === true) {
+//         addDarkMode()
+//     } else {
+//         RemoveDarkMode()
+//     }
+// }
+
+// btn.addEventListener("click", handleBtn);
+
+//SHORTER VERSION
 const btn = document.querySelector("button");
+const body = document.querySelector("body");
 let check = false;
-
-function addDarkMode() {
-    document.querySelector("body").classList.add("darkMode");
-    btn.textContent = "Light Mode";
-}
-
-function RemoveDarkMode() {
-    document.querySelector("body").classList.remove("darkMode");
-    btn.textContent = "Dark Mode";
-}
 
 function handleBtn() {
     check = !check
+    body.classList.toggle("darkMode");
     if (check === true) {
-        addDarkMode()
+        btn.textContent = "Light Mode";
     } else {
-        RemoveDarkMode()
+        btn.textContent = "Dark Mode";
     }
 }
 
-btn.addEventListener("click", handleBtn)
+btn.addEventListener("click", handleBtn);
